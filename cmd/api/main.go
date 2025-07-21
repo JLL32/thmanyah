bca2cd3811a09066a96c413ed0f557f32e392eeb
JALAL <eabduljalil@outlook.com>
2025-07-21 12:59:10 +0100

    Add basic HTTP server with graceful shutdown

---
+++ cmd/api/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+	"sync"
+)
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+	wg     sync.WaitGroup
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err := app.serve()
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
