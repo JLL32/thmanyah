81e760ab345950710fd2c19bec226afa1d160af9
JALAL <eabduljalil@outlook.com>
2025-07-21 21:41:50 +0100

    Add Postgres connection and configuration

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,11 +12,11 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.HandlerFunc(http.MethodGet, "/v1/videos", app.listVideosHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/videos", app.createVideoHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/videos/:id", app.showVideoHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/videos/:id", app.updateVideoHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/videos/:id", app.deleteVideoHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/videos", app.listVideosHandler)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
