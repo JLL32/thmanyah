c0e11ce8ac96d3d2dfa013a9a96ee8edf05f0085
JALAL <eabduljalil@outlook.com>
2025-07-21 19:01:44 +0100

    Add video resource routes and placeholder handlers

---
+++ cmd/api/videos.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (app *application) createVideoHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the createvideoHandler function
+}
+
+func (app *application) showVideoHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the showvideoHandler function
+}
+
+func (app *application) updateVideoHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the updatevideoHandler function
+}
+
+func (app *application) deleteVideoHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the deletevideoHandler function
+}
+
+func (app *application) listVideosHandler(w http.ResponseWriter, r *http.Request) {
+	// Implement the listVideosHandler function
+}
