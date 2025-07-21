024c32a11e35743286d9ad031c9f6529c5aa2da4
JALAL <eabduljalil@outlook.com>
2025-07-21 23:22:44 +0100

    Add panic recovery middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,5 +20,5 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
