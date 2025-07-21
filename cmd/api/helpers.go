b01d533dcf68fdc15aadba57504aba91665cd022
JALAL <eabduljalil@outlook.com>
2025-07-21 21:43:01 +0100

    impelement video get and post end points

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -16,12 +16,12 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) readIDParam(r *http.Request) (int64, error) {
+func (app *application) readIDParam(r *http.Request) (string, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id parameter")
+	id := params.ByName("id")
+	if id == "" {
+		return "", errors.New("invalid id parameter")
 	}
 
 	return id, nil
--- cmd/api/videos.go
+++ cmd/api/videos.go
@@ -1,13 +1,86 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/JLL32/thmanyah/internal/data"
+	"github.com/JLL32/thmanyah/internal/validator"
+)
 
 func (app *application) createVideoHandler(w http.ResponseWriter, r *http.Request) {
-	// Implement the createvideoHandler function
+	var input struct {
+		VideoID     string    `json:"video_id"`
+		Title       string    `json:"title"`
+		Description string    `json:"description"`
+		Type        string    `json:"type"`
+		Language    string    `json:"language"`
+		Length      int       `json:"length"`
+		PublishedAt time.Time `json:"published_at"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	video := &data.Video{
+		VideoID:     input.VideoID,
+		Title:       input.Title,
+		Description: input.Description,
+		Type:        input.Type,
+		Language:    input.Language,
+		Length:      input.Length,
+		PublishedAt: input.PublishedAt,
+	}
+
+	v := validator.New()
+	if data.ValidateVideo(v, video); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Videos.Insert(video)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/videos/%s", video.VideoID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"video": video}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 }
 
 func (app *application) showVideoHandler(w http.ResponseWriter, r *http.Request) {
-	// Implement the showvideoHandler function
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	video, err := app.models.Videos.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"video": video}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) updateVideoHandler(w http.ResponseWriter, r *http.Request) {
